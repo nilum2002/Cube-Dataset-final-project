a8b1c3e880ff25a43b436b056976119d91424a4c
nilum2002 <sachithyanilum@gmail.com>
2026-05-02 19:23:53 +0530

    update : new  training results updated

--- train/train.py
+++ train/train.py
@@ -1,35 +1,52 @@
 from ultralytics import YOLO
 
-# Load the nano model
+# # Load the nano model
 model = YOLO('yolov8n.pt')
 
-# # Train the model
-results = model.train(
-    data='/media/nilum/New_Volume/01.projects/Dataset-cubes/Robo-Games-4/data.yaml', 
+
+
+# # # Train the model
+# results = model.train(
+#     data='/media/nilum/New_Volume/01.projects/Dataset-cubes/Cube-Detection-Dataset-3/data.yaml', 
+#     epochs=100, 
+#     warmup_epochs=10,
+#     warmup_momentum=0.9,
+#     warmup_bias_lr=0.01,
+#     imgsz=640, 
+#     device='0', 
+#     lr0=1e-7,
+#     project='/media/nilum/New_Volume/01.projects/Dataset-cubes/runs', 
+#     single_cls = True,
+#     # freeze=10,
+#     weight_decay=1e-4,
+#     mixup=0.2,
+#     cos_lr = True)
+ 
+
+# # # Tune the model
+results = model.tune(
+    data='/media/nilum/New_Volume/01.projects/Dataset-cubes/Cube-Detection-Dataset-3/data.yaml', 
     epochs=100, 
     warmup_epochs=10,
     warmup_momentum=0.9,
     warmup_bias_lr=0.01,
     imgsz=640, 
     device='0', 
-    lr0=1e-5, 
+    lr0=1e-7,
     project='/media/nilum/New_Volume/01.projects/Dataset-cubes/runs', 
     single_cls = True,
     # freeze=10,
-    weight_decay=5e-4,
+    weight_decay=1e-4,
     mixup=0.2,
     cos_lr = True)
- 
-
-
 
 ######################################################
 #             Model Validation                       #
 ######################################################
 
 
-model = YOLO("/media/nilum/New_Volume/01.projects/Dataset-cubes/runs/train-14/weights/best.pt")
+# model = YOLO("/media/nilum/New_Volume/01.projects/Dataset-cubes/runs/train-16/weights/best.pt")
 
-metrics = model.val(data="/media/nilum/New_Volume/01.projects/Dataset-cubes/Robo-Games-4/data.yaml", split="val", save_dir = "/media/nilum/New_Volume/01.projects/Dataset-cubes/runs/val-12")
+# metrics = model.val(data="/media/nilum/New_Volume/01.projects/Dataset-cubes/Cube-Detection-Dataset-3/data.yaml", split="val", save_dir = "/media/nilum/New_Volume/01.projects/Dataset-cubes/runs/val-15")
 
-print(metrics)
+# print(metrics)
