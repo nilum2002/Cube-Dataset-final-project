c164ddde89ac44c4fbf85d3c06194034e70b46b7
nilum2002 <sachithyanilum@gmail.com>
2026-05-11 18:36:39 +0530

    update: results for new dataset

--- train/train.py
+++ train/train.py
@@ -25,7 +25,7 @@ model = YOLO('yolov8n.pt')
 
 # # # Tune the model
 results = model.tune(
-    data='/media/nilum/New_Volume/01.projects/Dataset-cubes/Cube-Detection-Dataset-3/data.yaml', 
+    data='/home/intellisense05/Nilum/Cube-Dataset-final-project/Robo-Games-5/data.yaml', 
     epochs=100, 
     warmup_epochs=10,
     warmup_momentum=0.9,
@@ -33,7 +33,7 @@ results = model.tune(
     imgsz=640, 
     device='0', 
     lr0=1e-7,
-    project='/media/nilum/New_Volume/01.projects/Dataset-cubes/runs', 
+    project='/home/intellisense05/Nilum/Cube-Dataset-final-project/runs2', 
     single_cls = True,
     # freeze=10,
     weight_decay=1e-4,
